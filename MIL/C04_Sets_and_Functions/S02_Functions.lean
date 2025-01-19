27d7051255e869b98c98eb37ac707a224f9dedb6
nnarek <n.nazaretyan.5@gmail.com>
2025-01-19 19:01:21 +0000

    finished all exercises from Set and Functions Chapter

--- MIL/C04_Sets_and_Functions/S02_Functions.lean
+++ MIL/C04_Sets_and_Functions/S02_Functions.lean
@@ -34,66 +34,156 @@ example : s ⊆ f ⁻¹' (f '' s) := by
   use x, xs
 
 example : f '' s ⊆ v ↔ s ⊆ f ⁻¹' v := by
-  sorry
+  constructor
+  · intro h x xs; simp at *; exact h xs
+  · rintro h x ⟨a,as,hfax⟩; have h' := h as; simp[*] at *; assumption
 
 example (h : Injective f) : f ⁻¹' (f '' s) ⊆ s := by
-  sorry
+  intro _ ⟨_,_,fafx⟩
+  rw [<-h fafx]
+  assumption
 
 example : f '' (f ⁻¹' u) ⊆ u := by
-  sorry
+  intro _ ⟨_,_,_⟩
+  simp[*] at *
+  assumption
 
 example (h : Surjective f) : u ⊆ f '' (f ⁻¹' u) := by
-  sorry
+  intro x xu
+  rcases h x with ⟨a,fax⟩
+  use a
+  simp [*] at *
 
 example (h : s ⊆ t) : f '' s ⊆ f '' t := by
-  sorry
+  intro x ⟨a,as,fax⟩
+  use a
+  simp [*] at *
+  exact h as
 
 example (h : u ⊆ v) : f ⁻¹' u ⊆ f ⁻¹' v := by
-  sorry
+  intro x xfu
+  exact h xfu
 
 example : f ⁻¹' (u ∪ v) = f ⁻¹' u ∪ f ⁻¹' v := by
-  sorry
+  ext x
+  constructor
+  repeat
+    rintro (h | h) <;> simp
+    · left; assumption
+    · right; assumption
 
 example : f '' (s ∩ t) ⊆ f '' s ∩ f '' t := by
-  sorry
+  rintro x ⟨a,⟨_,_⟩,rfl⟩
+  simp
+  constructor <;> use a
+
 
 example (h : Injective f) : f '' s ∩ f '' t ⊆ f '' (s ∩ t) := by
-  sorry
+  rintro x ⟨⟨a,at',rfl⟩,⟨b,bt,fafb⟩⟩
+  simp at *
+  use a
+  rw [h fafb] at bt
+  trivial
 
 example : f '' s \ f '' t ⊆ f '' (s \ t) := by
-  sorry
+  rintro x ⟨⟨a,_,rfl⟩,nfxfa⟩
+  simp at *
+  use a
+  simp
+  constructor
+  · assumption
+  · intro h; exact nfxfa a h rfl
 
 example : f ⁻¹' u \ f ⁻¹' v ⊆ f ⁻¹' (u \ v) := by
-  sorry
+  rintro x ⟨_,_⟩
+  simp at *
+  constructor <;> assumption
 
 example : f '' s ∩ v = f '' (s ∩ f ⁻¹' v) := by
-  sorry
+  ext x
+  constructor
+  · rintro ⟨⟨a,_,rfl⟩,_⟩
+    simp
+    use a
+  · rintro ⟨a,⟨_,_⟩,rfl⟩
+    simp at *
+    constructor
+    · use a
+    · assumption
+
 
 example : f '' (s ∩ f ⁻¹' u) ⊆ f '' s ∩ u := by
-  sorry
+  rintro x ⟨a,⟨_,_⟩,rfl⟩
+  simp at *
+  constructor
+  · use a
+  · assumption
 
 example : s ∩ f ⁻¹' u ⊆ f ⁻¹' (f '' s ∩ u) := by
-  sorry
+  rintro x ⟨_,_⟩
+  simp at *
+  constructor
+  · use x
+  · assumption
 
 example : s ∪ f ⁻¹' u ⊆ f ⁻¹' (f '' s ∪ u) := by
-  sorry
+  rintro x (_|_) <;> simp
+  · left; use x
+  · right; assumption
 
 variable {I : Type*} (A : I → Set α) (B : I → Set β)
 
 example : (f '' ⋃ i, A i) = ⋃ i, f '' A i := by
-  sorry
+  ext x
+  constructor
+  · rintro ⟨x,⟨_,⟨i,rfl⟩,_⟩,rfl⟩
+    simp at *
+    use i,x
+  · rintro ⟨_,⟨i,rfl⟩,x,_,rfl⟩
+    simp at *
+    use x
+    simp
+    use i
 
 example : (f '' ⋂ i, A i) ⊆ ⋂ i, f '' A i := by
-  sorry
+  rintro _ ⟨x,xai,rfl⟩ _ ⟨i,rfl⟩
+  simp at *
+  use x
+  simp
+  exact xai i
+
 
 example (i : I) (injf : Injective f) : (⋂ i, f '' A i) ⊆ f '' ⋂ i, A i := by
-  sorry
+  rintro _ h
+  simp at h
+  rcases h i with ⟨a,_,rfl⟩
+  simp
+  use a
+  simp
+  intro i'
+  rcases h i' with ⟨b,_,fbfa⟩
+  apply injf at fbfa
+  simp[*] at *
+  assumption
 
 example : (f ⁻¹' ⋃ i, B i) = ⋃ i, f ⁻¹' B i := by
-  sorry
+  ext x
+  constructor
+  · rintro ⟨_,⟨i,rfl⟩,_⟩
+    simp at *
+    use i
+  · rintro ⟨_,⟨i,rfl⟩,_⟩
+    simp at *
+    use i
 
 example : (f ⁻¹' ⋂ i, B i) = ⋂ i, f ⁻¹' B i := by
-  sorry
+  ext _
+  constructor
+  repeat
+  · rintro h i ⟨_,rfl⟩
+    simp at *
+    apply h
+
 
 example : InjOn f s ↔ ∀ x₁ ∈ s, ∀ x₂ ∈ s, f x₁ = f x₂ → x₁ = x₂ :=
   Iff.refl _
@@ -123,16 +213,48 @@ example : range exp = { y | y > 0 } := by
   rw [exp_log ypos]
 
 example : InjOn sqrt { x | x ≥ 0 } := by
-  sorry
+  intro x xp y yp sxeqsy
+  simp at *
+  calc
+    x = (√x) ^ 2 := Eq.symm (sq_sqrt xp)
+    _ = (√y) ^ 2 := by congr
+    _ = y := sq_sqrt yp
 
 example : InjOn (fun x ↦ x ^ 2) { x : ℝ | x ≥ 0 } := by
-  sorry
+  intro x xp y yp x2eqy2
+  simp at *
+  calc
+    x = √(x ^ 2) := Eq.symm (sqrt_sq xp)
+    _ = √(y ^ 2) := by congr
+    _ = y := sqrt_sq yp
+
 
 example : sqrt '' { x | x ≥ 0 } = { y | y ≥ 0 } := by
-  sorry
+  ext x
+  constructor
+  · rintro ⟨y,yp,syeqx⟩
+    simp at *
+    rw [←syeqx]
+    exact sqrt_nonneg y
+  · rintro xp
+    simp at *
+    use x ^ 2
+    constructor
+    · exact sq_nonneg x
+    · exact sqrt_sq xp
+
 
 example : (range fun x ↦ x ^ 2) = { y : ℝ | y ≥ 0 } := by
-  sorry
+  ext x
+  constructor
+  · intro ⟨y,y2eqx⟩
+    simp at *
+    rw [<-y2eqx]
+    exact sq_nonneg y
+  · intro xp
+    simp at *
+    use sqrt x
+    apply sq_sqrt xp
 
 end
 
@@ -155,6 +277,8 @@ open Classical
 def inverse (f : α → β) : β → α := fun y : β ↦
   if h : ∃ x, f x = y then Classical.choose h else default
 
+#print inverse
+
 theorem inverse_spec {f : α → β} (y : β) (h : ∃ x, f x = y) : f (inverse f y) = y := by
   rw [inverse, dif_pos h]
   exact Classical.choose_spec h
@@ -164,10 +288,31 @@ variable (f : α → β)
 open Function
 
 example : Injective f ↔ LeftInverse (inverse f) f :=
-  sorry
+by
+  constructor
+  · intro injf x
+    simp at *
+    apply injf
+    apply inverse_spec
+    use x
+  · intro h x y fxfy
+    unfold LeftInverse at h
+    rw [←h x,←h y,fxfy]
+
 
 example : Surjective f ↔ RightInverse (inverse f) f :=
-  sorry
+by
+  constructor
+  · unfold Surjective
+    intro surjf x
+    apply inverse_spec
+    rcases surjf x with ⟨a,fax⟩
+    use a
+  · unfold RightInverse
+    unfold LeftInverse
+    intro h x
+    use inverse f x
+    apply h x
 
 end
 
@@ -183,10 +328,13 @@ theorem Cantor : ∀ f : α → Set α, ¬Surjective f := by
     intro h'
     have : j ∉ f j := by rwa [h] at h'
     contradiction
+
   have h₂ : j ∈ S
-  sorry
-  have h₃ : j ∉ S
-  sorry
+  · unfold S at *
+    simp at *
+    assumption
+
+  rw [h] at *
   contradiction
 
 -- COMMENTS: TODO: improve this
--- MIL/C04_Sets_and_Functions/S03_The_Schroeder_Bernstein_Theorem.lean
+++ MIL/C04_Sets_and_Functions/S03_The_Schroeder_Bernstein_Theorem.lean
@@ -28,10 +28,15 @@ theorem sb_right_inv {x : α} (hx : x ∉ sbSet f g) : g (invFun g x) = x := by
     rw [sbSet, mem_iUnion]
     use 0
     rw [sbAux, mem_diff]
-    sorry
+    simp at *
+    assumption
+
   have : ∃ y, g y = x := by
-    sorry
-  sorry
+    simp at *
+    exact this
+
+  apply invFun_eq this
+
 
 theorem sb_injective (hf : Injective f) : Injective (sbFun f g) := by
   set A := sbSet f g with A_def
@@ -50,15 +55,24 @@ theorem sb_injective (hf : Injective f) : Injective (sbFun f g) := by
       rw [if_pos x₁A, if_neg x₂nA] at hxeq
       rw [A_def, sbSet, mem_iUnion] at x₁A
       have x₂eq : x₂ = g (f x₁) := by
-        sorry
+        simp[*] at *
+        symm
+        apply sb_right_inv
+        assumption
       rcases x₁A with ⟨n, hn⟩
       rw [A_def, sbSet, mem_iUnion]
       use n + 1
       simp [sbAux]
       exact ⟨x₁, hn, x₂eq.symm⟩
-    sorry
+    simp[x₁A,x₂A] at hxeq
+    exact hf hxeq
+
   push_neg at xA
-  sorry
+  simp[*] at *
+  have : g (invFun g x₁) = g (invFun g x₂) := by rw [hxeq]
+  rw [sb_right_inv _ _ xA.1,sb_right_inv _ _ xA.2] at this
+  assumption
+
 
 theorem sb_surjective (hg : Injective g) : Surjective (sbFun f g) := by
   set A := sbSet f g with A_def
@@ -77,7 +91,11 @@ theorem sb_surjective (hg : Injective g) : Surjective (sbFun f g) := by
       exact ⟨n, xmem⟩
     simp only [h_def, sbFun, if_pos this]
     exact hg hx
-  sorry
+  · simp [*] at gyA ⊢
+    unfold sbFun
+    use g y
+    simp[gyA]
+    exact leftInverse_invFun hg _
 
 end
 
